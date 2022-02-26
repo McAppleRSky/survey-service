035a887fc72b04c499d7f46a35c14e92b06b0631
McAppleRSky <56290240+McAppleRSky@users.noreply.github.com>
2022-02-26 15:07:37 +0300

    add query person's answer

--- service/src/main/java/solv/fact/repository/AnswerRepositoryQuery.java
+++ service/src/main/java/solv/fact/repository/AnswerRepositoryQuery.java
@@ -1,6 +1,5 @@
 package solv.fact.repository;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -26,5 +25,14 @@ public interface AnswerRepositoryQuery extends CrudRepository<Answer, Integer> {
     void createAnswerText(
             @Param("textAnswer") String text_answer,
             @Param("participationId") Integer participation_id );
+
+    @Query(value =
+            "SELECT s.title_survey, q.title_question, v.ti" +
+            "  FROM participation p JOIN survey s ON p.survey_id = s.survey_id \n" +
+            "  JOIN question q ON p.question_id = q.question_id \n" +
+            "  JOIN answer a ON p.answer_id = a.answer_id \n" +
+            " WHERE person_id = :personId \n",
+            nativeQuery = true)
+    Object findAllByPersonId(@Param("personId") Integer participationId);
 
 }
